70dcbdda11b14785c486d03d5c3a316176b75887
Toshiaki Inahata <darwin49@gmail.com>
2024-07-26 20:41:04 +0900

    fix package

--- Jakefile.js
+++
@@ -1,7 +0,0 @@
-const { task, desc } = require('jake');
-
-task('clean', async () => {
-  const { deleteSync } = await import('del');
-
-  deleteSync(['dist/**/*.js', 'dist/**/*.js.map', 'dist/**/*.d.ts', 'dist/**/*.d.ts.map', '*.tgz']);
-});
